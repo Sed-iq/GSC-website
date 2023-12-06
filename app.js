8d2f0ce59af40d305aec6aa7329623d61f5023e8
Sediq <sediqabdullahi01@gmail.com>
2023-12-06 05:25:52 +0100

    Added an update

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-import express from "express";
-import mongoose from "mongoose";
+// import express from "express";
+// import mongoose from "mongoose";
 import dotenv from "dotenv";
-import flash from "express-flash";
-import session from "express-session";
-import cookie from "cookie-parser";
-import { fileURLToPath } from "url";
-import { dirname, join as j } from "path";
+// import flash from "express-flash";
+// import session from "express-session";
+// import cookie from "cookie-parser";
+// import { fileURLToPath } from "url";
+// import { dirname, join as j } from "path";
 
-import Router from "./modules/router.js";
+// import Router from "./modules/router.js";
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// const app = express();
+// app.use(cookie(process.env.SECRET));
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/public", express.static(j(__dirname + "/public")));
+// app.use(
+//   session({
+//     secret: process.env.SECRET,
+//     resave: true,
+//     saveUninitialized: false,
+//   })
+// );
+// app.use(flash());
+// app.use(Router);
+// mongoose
+//   .connect(process.env.DB)
+//   .then(() => {
+//     app.listen(
+//       process.env.PORT,
+//       console.log("GSC running http://localhost:5000")
+//     );
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
+import express from "express";
 const app = express();
-app.use(cookie(process.env.SECRET));
-app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(j(__dirname + "/public")));
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: false,
-  })
-);
-app.use(flash());
-app.use(Router);
-mongoose
-  .connect(process.env.DB)
-  .then(() => {
-    app.listen(
-      process.env.PORT,
-      console.log("GSC running http://localhost:5000")
-    );
-  })
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.listen(process.env.PORT, console.log("running"));
